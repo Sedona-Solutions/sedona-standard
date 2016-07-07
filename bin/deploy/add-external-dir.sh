472a9438acf3f945fe3d5c3bd691a660a768088c
Jean Pasdeloup <jpasdeloup@sedona.fr>
2016-07-07 17:50:58 +0200

    improve bins

--- bin/deploy/add-external-dir.sh
+++ bin/deploy/add-external-dir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
+if [ $# -lt 2 ]; then
     echo "Usage add-external-dir <name> <local_dir> <external_dir>"
     exit 1
 fi
@@ -12,13 +12,15 @@ external_dir=${3// }
 if [ ! -z $external_dir ]; then
     echo " using external $name in $external_dir"
     if [ ! -d $external_dir ]; then
-        sudo mkdir -p $external_dir
+        mkdir -p $external_dir
         bin/deploy/add-permissions.sh $external_dir
     fi
     ln -nsf $external_dir $local_dir
 else
     echo " no global $name, create locally in $local_dir"
-    mkdir -p $local_dir
-    bin/deploy/add-permissions.sh $local_dir
+    if [ ! -d $local_dir ]; then
+        mkdir -p $local_dir
+        bin/deploy/add-permissions.sh $local_dir
+    fi
 fi
 
--- bin/deploy/add-permissions.sh
+++ bin/deploy/add-permissions.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+CLIUSER=`whoami`
+
+if [ $HTTPDUSER = $CLIUSER ]; then
+    # Same user of http and cli, no need to add custom rights
+    exit 0
+fi
+
+[[ 'root' = $CLIUSER ]] && sudo='' || sudo='sudo '
+
 if [ -n "$1" ]; then
-    sudo setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX $1
-    sudo setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx $1
+    $sudo setfacl -R -m u:$HTTPDUSER:rwX -m u:$CLIUSER:rwX $1
+    $sudo setfacl -dR -m u:$HTTPDUSER:rwX -m u:$CLIUSER:rwX $1
 else
     echo "ERROR: path missing"
     echo " usage: add-permissions <path>"
